6e585ea059d4b4ebdc19ac8f37c8e18d3def3438
John Sison <132543192+jls777@users.noreply.github.com>
2025-10-23 01:07:13 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Register from "./Register";
+
+function App() {
+  return (
+    <main>
+      <Register />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Register.jsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <div>test</div>;
+}
+
+export default Register;
